0dddddf29118712d963bcc6608da69de81e23f9c
ZorAnderius <gabriel.zorander@gmail.com>
2024-03-27 00:54:30 +0000

    add slider and fixed issues with missing src in index.html

--- src/main.js
+++ src/main.js
@@ -26,3 +26,40 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  centeredSlides: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  slidesPerView: 1,
+
+  breakpoints: {
+    375: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    }
+  }
+});
